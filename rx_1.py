35522e32428b8493dcbfb2c71dc3a1377b94b5d7
Fortunato <33538533+fortunatoramirez@users.noreply.github.com>
2022-07-14 15:36:57 -0700

    Update rx_1.py

--- rx_1.py
+++ rx_1.py
@@ -2,7 +2,7 @@ from socketIO_client import SocketIO
 
 # Conectando al socket del Servidor
 print("Conectando al Servidor...")
-socketIO = SocketIO('201.174.122.202',5001)
+socketIO = SocketIO('ip.del.ser.vidor',5001)
 print("Conectado al Servidor.")
 
 def rutina(*args):
